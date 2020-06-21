a2333e249bf077a902d9cd559805f1d1b68cc6c7
Janshai <jack.r.s.adams@gmail.com>
2020-06-21 23:37:50 +0100

    create empty XCode project and add cocoapods and add Firebase Analytics Pod. Add Firebase setup code

---
+++ Goalie/Goalie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Goalie
+//
+//  Created by Jack Adams on 21/06/2020.
+//  Copyright © 2020 Jack Adams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
