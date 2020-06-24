b0321ea035a9ce2b1550dee842d8d0b1b144d17f
Janshai <jack.r.s.adams@gmail.com>
2020-06-24 12:22:39 +0100

    setup root tab bar controller in the storyboard and creat the files for the settings VC

---
+++ Goalie/Goalie/SettingsBaseTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SettingsBaseTableViewController.swift
+//  Goalie
+//
+//  Created by Jack Adams on 24/06/2020.
+//  Copyright © 2020 Jack Adams. All rights reserved.
+//
+
+import UIKit
+import Eureka
+
+class SettingsBaseTableViewController: FormViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
