5793182051ebe9cb43311f3479081321c5c776f6
Janshai <jack.r.s.adams@gmail.com>
2020-06-24 13:40:23 +0100

    add sign out implementation. Todo: add activity indicator, stop log in pop up auto launching, abstract to view model

--- Goalie/Goalie/SettingsBaseTableViewController.swift
+++ Goalie/Goalie/SettingsBaseTableViewController.swift
@@ -22,6 +22,28 @@ class SettingsBaseTableViewController: FormViewController {
         
         self.form +++ ButtonRow() { row in
             row.title = "Log Out"
+            row.onCellSelection() { cell, row in
+                let auth = AuthManager.defaultAuthProvider
+                auth.logOut() { error in
+                    if let e = error {
+                        print(e.localizedDescription)
+                        let alert = UIAlertController(title: "Error", message: "Oops, there was an error while signing you out. Please try again.", preferredStyle: UIAlertController.Style.alert)
+                        alert.addAction(UIAlertAction(title: "Close", style: UIAlertAction.Style.default, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+                    } else {
+                        if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() {
+                            guard let window = self.view.window else {
+                                return
+                            }
+                            window.rootViewController = vc
+                            let options: UIView.AnimationOptions = .transitionCrossDissolve
+                            let duration: TimeInterval = 0.3
+                            
+                            UIView.transition(with: window, duration: duration, options: options, animations: {}, completion: nil)
+                        }
+                    }
+                }
+            }
         }
 
         // Do any additional setup after loading the view.
